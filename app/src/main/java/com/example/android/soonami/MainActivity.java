e60e0ad7164507c5b19d0494b2ccd365931048ad
AlexanderPokidin <a.pokidin@gmail.com>
2018-10-09 11:43:54 +0300

    Errors handled

--- app/src/main/java/com/example/android/soonami/MainActivity.java
+++ app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -167,10 +167,12 @@ public class MainActivity extends AppCompatActivity {
                 if (urlConnection.getResponseCode() == 200){
                     inputStream = urlConnection.getInputStream();
                     jsonResponse = readFromStream(inputStream);
+                } else {
+                    Log.e(LOG_TAG, "Error response code " + urlConnection.getResponseCode());
                 }
 
             } catch (IOException e) {
-                // TODO: Handle the exception
+                Log.e(LOG_TAG, e.toString());
             } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
